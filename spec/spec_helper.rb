74e55c4e1f2eddf0affe27e741547ddfef2e5173
David Thompson <dtt101@gmail.com>
2014-06-16 14:07:48 +0100

    Move dummy app and configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,21 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 #
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
