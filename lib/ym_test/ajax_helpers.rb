d96905f2a09a4c6a9ed113c228b485347599654c
Greg Woodcock <greg@yoomee.com>
2015-02-24 13:04:23 +0000

    Allow wait_for_ajax to rescue from not supported error

--- lib/ym_test/ajax_helpers.rb
+++ lib/ym_test/ajax_helpers.rb
@@ -1,8 +1,12 @@
 module YmTest
   module AjaxHelpers
     def wait_for_ajax
-      Timeout.timeout(Capybara.default_wait_time) do
-        loop until finished_all_ajax_requests?
+      begin
+        Timeout.timeout(Capybara.default_wait_time) do
+          loop until finished_all_ajax_requests?
+        end
+      rescue Capybara::NotSupportedByDriverError => e
+        puts "Javascript not enabled, not waiting for script to finish"
       end
     end
 
